6a646e622ee4a25da938e84fdfc67ae14c117d0c
Tao Ren <taoren.physics@icloud.com>
2021-07-25 17:16:46 -0700

    delete old files

--- WebApp/CommentInsights/analysis/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from analysis import views
-
-urlpatterns = [
-    path('home/', views.homepage, name='home'),
-    path('upload/', views.upload_file, name='upload'),
-]
--- WebApp_deployment/CommentInsights/CommentInsights/secret.py
+++
@@ -1,3 +0,0 @@
-credential_info = {
-
-}
